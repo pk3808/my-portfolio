537c316800766ca679f0423d8dd09452acd002f6
Piyush <piyush@ibarts.in>
2024-11-05 17:33:18 +0530

    texas changes

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import NavBar from '../components/NavBar';
+import React, { Suspense } from 'react';
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import Scene from '../../public/Scene.jsx';
 
 const Home = () => {
   return (
     <div className="container mx-auto py-16 min-h-screen">
-      <h1 className="text-4xl font-bold text-center">Welcome to My Portfolio</h1>
-      <p className="text-center mt-4 text-lg">Explore my work and get to know me!</p>
+      <Canvas camera={{ position: [0, 1, 3], fov: 50 }}> 
+        <OrbitControls minDistance={1} maxDistance={-30} />
+        <ambientLight  /> 
+        <directionalLight position={[0, 10, 5]} intensity={1} />
+        <Suspense fallback={null}>
+          <Scene />
+        </Suspense>
+      </Canvas>
     </div>
   );
 };
